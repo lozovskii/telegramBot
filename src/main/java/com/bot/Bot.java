53a0a446c5c5f465dcb356cafb504a504a223788
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-04-05 01:09:31 +0300

    Initial commit

---
+++ src/main/java/com/bot/Bot.java
@@ -0,0 +1,43 @@
+package com.bot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        Message msg = update.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Hello, world! This is simple bot!");
+        }else if(txt.equals("yo") || txt.equals("hello") || txt.equals("hi")){
+            sendMsg(msg, "Bonjour!");
+        }else if(txt.equals("What is your name?") || txt.equals("what is your name?")){
+            sendMsg(msg, "My name test503503Bot");
+        }else{
+            sendMsg(msg, "Sorry, but i don't understand :( \nHope study it");
+        }
+    }
+
+    public String getBotUsername() {
+        return Config.botName;
+    }
+
+    public String getBotToken() {
+        return Config.botToken;
+    }
+
+    private void sendMsg(Message msg, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(msg.getChatId());
+        message.setText(text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/bot/Main.java
@@ -0,0 +1,20 @@
+package com.bot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Config.loadProp();
+        ApiContextInitializer.init();
+        TelegramBotsApi botApi = new TelegramBotsApi();
+        try {
+            botApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
