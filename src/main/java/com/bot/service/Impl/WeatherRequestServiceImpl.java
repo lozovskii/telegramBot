3634a004c1363843e5e93f877e4042ec966bc729
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-01 18:56:30 +0300

    added WeatherRequestService

---
+++ src/main/java/com/bot/service/Impl/WeatherRequestServiceImpl.java
@@ -0,0 +1,51 @@
+package com.bot.service.Impl;
+
+import com.bot.model.CityAnswerModel;
+import com.bot.service.WeatherRequestService;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+public class WeatherRequestServiceImpl implements WeatherRequestService {
+    @Override
+    public CityAnswerModel getWeather(String cityId) throws IOException {
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?id=" + cityId +
+                "&units=metric&appid=0b9500b929ea466a01d56bf6f86441af");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        JSONObject jsonResponse = new JSONObject(response.toString());
+        CityAnswerModel cityAnswerModel = new CityAnswerModel();
+        cityAnswerModel.setName(jsonResponse.get("name").toString());
+        cityAnswerModel.setDescription(jsonResponse.getJSONArray("weather")
+                .getJSONObject(0).get("description").toString());
+        cityAnswerModel.setTemp(jsonResponse.getJSONObject("main")
+                .get("temp").toString());
+        cityAnswerModel.setPressure(jsonResponse.getJSONObject("main")
+                .get("pressure").toString());
+        cityAnswerModel.setHumidity(jsonResponse.getJSONObject("main")
+                .get("humidity").toString());
+        try {
+            cityAnswerModel.setVisibility(jsonResponse.get("visibility").toString());
+        } catch (JSONException e) {
+            cityAnswerModel.setVisibility(null);
+        }
+        cityAnswerModel.setWindSpeed(jsonResponse.getJSONObject("wind")
+                .get("speed").toString());
+        return cityAnswerModel;
+    }
+}
---
+++ src/main/java/com/bot/service/WeatherRequestService.java
@@ -0,0 +1,12 @@
+package com.bot.service;
+
+import com.bot.model.CityAnswerModel;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public interface WeatherRequestService {
+
+    CityAnswerModel getWeather(String cityId) throws IOException;
+
+}
