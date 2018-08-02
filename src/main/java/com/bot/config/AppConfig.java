b9a12744fa97860e437522d3d145d9eb825fa5b3
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-02 16:21:19 +0300

    increased heap size to 471859200

--- src/main/java/com/bot/config/AppConfig.java
+++ src/main/java/com/bot/config/AppConfig.java
@@ -22,7 +22,7 @@ public class AppConfig {
     public JavaSparkContext sc(){
         SparkConf sparkConf = new SparkConf()
                 .setAppName("SparkNameTest")
-                .setMaster("local[*]");
+                .setMaster("local[*]").set("spark.driver.memory", "471859200");
         return new JavaSparkContext(sparkConf);
     }
 
