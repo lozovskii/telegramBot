b7f0600530a2ffdb96459e9a8c8d00e2dc9b2f2d
Bohdan Bachkala <redowlave@gmail.com>
2018-08-03 10:49:05 +0300

    Update WeatherServiceImpl.java

--- src/main/java/com/bot/service/Impl/WeatherServiceImpl.java
+++ src/main/java/com/bot/service/Impl/WeatherServiceImpl.java
@@ -21,12 +21,15 @@ public class WeatherServiceImpl implements WeatherService {
     @Value("${jsonPath}")
     private String jsonPath;
 
+    // consider constructor/setter autowire
     @Autowired
     private JavaSparkContext sc;
 
+    // why throws NoSuchCityException | it is a custom exception make IT runtime
     @Override
     public String getCityId(String city) throws NoSuchCityException {
         SQLContext sqlContext = SQLContext.getOrCreate(sc.sc()).newSession();
+        // make a separate method for city name manipulation its just polluting logic of this mehtod
         String correctCityNameFormat =  Arrays.stream(city.split(" "))
                 .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1, i.length()))
                 .collect(Collectors.joining(" "));
@@ -41,6 +44,7 @@ public class WeatherServiceImpl implements WeatherService {
         } catch (NoSuchElementException e) {
             throw new NoSuchCityException("This city name is not correct or city with this name does not exist!");
         }
+        // some dark magic | why is this 2 what is happening
         return (((Row) rowEntity).get(2).toString());
     }
 }
