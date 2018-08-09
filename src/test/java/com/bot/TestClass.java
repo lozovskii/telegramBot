0df8268ee2ce27caa741ff5c48c20cfe98c684d2
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-09 22:45:50 +0300

    added parse test for crypto currency

--- src/test/java/com/bot/TestClass.java
+++ src/test/java/com/bot/TestClass.java
@@ -2,8 +2,8 @@ package com.bot;
 
 import com.bot.config.AppConfig;
 import com.bot.model.CityAnswerModel;
+import com.bot.model.CryptoCurrencyModel;
 import com.bot.service.MessageService;
-import com.bot.service.WeatherRequestService;
 import com.bot.service.WeatherService;
 import com.bot.util.exception.NoSuchCityException;
 import org.apache.logging.log4j.LogManager;
@@ -25,6 +25,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
@@ -39,9 +42,6 @@ public class TestClass {
 
     @Autowired
     private MessageService messageService;
-
-    @Autowired
-    private WeatherRequestService weatherRequestService;
 
     @Value("${jsonPath}")
     private String jsonPath;
@@ -122,7 +122,7 @@ public class TestClass {
     @Ignore
     @Test
     public void getWeather() throws IOException {
-        CityAnswerModel weather = weatherRequestService.getWeather("703448");
+        CityAnswerModel weather = weatherService.getWeather("703448");
 
         log.debug(weather);
     }
@@ -132,6 +132,90 @@ public class TestClass {
     public void getAnswer() throws IOException {
         String kiev = messageService.getAnswer("Kiev");
         log.debug(kiev);
+    }
+
+    @Ignore
+    @Test
+    public void testParsCryptoCurrency() throws JSONException {
+        String response = "{\n" +
+                "    \"data\": {\n" +
+                "        \"1\": {\n" +
+                "            \"id\": 1,\n" +
+                "            \"name\": \"Bitcoin\",\n" +
+                "            \"symbol\": \"BTC\",\n" +
+                "            \"website_slug\": \"bitcoin\",\n" +
+                "            \"rank\": 1,\n" +
+                "            \"circulating_supply\": 17199262.0,\n" +
+                "            \"total_supply\": 17199262.0,\n" +
+                "            \"max_supply\": 21000000.0,\n" +
+                "            \"quotes\": {\n" +
+                "                \"USD\": {\n" +
+                "                    \"price\": 6430.36791515,\n" +
+                "                    \"volume_24h\": 4651816716.43908,\n" +
+                "                    \"market_cap\": 110597582529.0,\n" +
+                "                    \"percent_change_1h\": 2.2,\n" +
+                "                    \"percent_change_24h\": -1.26,\n" +
+                "                    \"percent_change_7d\": -15.26\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"last_updated\": 1533822991\n" +
+                "        },\n" +
+                "        \"2246\": {\n" +
+                "            \"id\": 2246,\n" +
+                "            \"name\": \"CyberMiles\",\n" +
+                "            \"symbol\": \"CMT\",\n" +
+                "            \"website_slug\": \"cybermiles\",\n" +
+                "            \"rank\": 100,\n" +
+                "            \"circulating_supply\": 700298400.0,\n" +
+                "            \"total_supply\": 1000000000.0,\n" +
+                "            \"max_supply\": null,\n" +
+                "            \"quotes\": {\n" +
+                "                \"USD\": {\n" +
+                "                    \"price\": 0.0919244418,\n" +
+                "                    \"volume_24h\": 10698513.4442638,\n" +
+                "                    \"market_cap\": 64374540.0,\n" +
+                "                    \"percent_change_1h\": 3.1,\n" +
+                "                    \"percent_change_24h\": -3.29,\n" +
+                "                    \"percent_change_7d\": -21.0\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"last_updated\": 1533823000\n" +
+                "        }" +
+                "    },\n" +
+                "    \"metadata\": {\n" +
+                "        \"timestamp\": 1533822549,\n" +
+                "        \"num_cryptocurrencies\": 1784,\n" +
+                "        \"error\": null\n" +
+                "    }\n" +
+                "}";
+        JSONObject jsonResponse = new JSONObject(response);
+        JSONObject data = jsonResponse.getJSONObject("data");
+        Iterator data1 = jsonResponse.getJSONObject("data").keys();
+        List<String> keys = new ArrayList<>();
+        List<CryptoCurrencyModel> responseCurrencyList = new ArrayList<>();
+        while(data1.hasNext()){
+            keys.add(data1.next().toString());
+        }
+        for (String key: keys){
+            JSONObject keyElement = jsonResponse.getJSONObject("data").getJSONObject(key);
+            JSONObject quotes = keyElement.getJSONObject("quotes");
+            Iterator quotesKeys = quotes.keys();
+            String currency = null;
+            while(quotesKeys.hasNext()){
+                currency = quotesKeys.next().toString();
+            }
+            String price = quotes.getJSONObject(currency).get("price").toString();
+            CryptoCurrencyModel cryptoCurrencyModel = new CryptoCurrencyModel.CryptoCurrencyModelBuilder(
+                    keyElement.get("name").toString(), price, currency)
+                    .id(key)
+                    .symbol(keyElement.get("symbol").toString())
+                    .rank(keyElement.get("rank").toString())
+                    .build();
+            responseCurrencyList.add(cryptoCurrencyModel);
+        }
+        System.out.println(responseCurrencyList);
+        Assert.assertTrue(responseCurrencyList.size() > 0);
+//        System.out.println(jsonResponse.getJSONObject("data").getJSONObject("1").get("circulating_supply").toString());
     }
 
 }
