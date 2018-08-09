9f7ce89b854eed9a66f5868eb182024071b0d53f
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-09 18:05:42 +0300

    created webService service

---
+++ src/main/java/com/bot/service/Impl/WebServiceImpl.java
@@ -0,0 +1,32 @@
+package com.bot.service.Impl;
+
+import com.bot.service.WebService;
+import com.bot.util.exception.NoConnection;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+public class WebServiceImpl implements WebService {
+    @Override
+    public String getResponse(URL url) {
+        try {
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } catch (IOException e) {
+            throw new NoConnection("Failed to connection. ", e);
+        }
+    }
+}
---
+++ src/main/java/com/bot/service/WebService.java
@@ -0,0 +1,9 @@
+package com.bot.service;
+
+import java.net.URL;
+
+public interface WebService {
+
+    String getResponse(URL url);
+
+}
