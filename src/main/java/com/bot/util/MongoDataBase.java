c0af56712064f6449e61debdc69ff39baef67601
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-14 00:33:39 +0300

    added utility class for get MongoDB

---
+++ src/main/java/com/bot/util/MongoDataBase.java
@@ -0,0 +1,21 @@
+package com.bot.util;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MongoDataBase {
+
+    @Value("${dataBase}")
+    private String DATA_BASE;
+
+    @Bean
+    public MongoDatabase getDataBase() {//db
+        MongoClient mongoClient = new MongoClient();
+        return mongoClient.getDatabase(DATA_BASE);
+    }
+
+}
