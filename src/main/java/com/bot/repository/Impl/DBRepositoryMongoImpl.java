a5c74ba20d6ce964ac96c621d884ca6b431529a1
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2018-08-16 20:23:29 +0300

    #8 Added body methods of searchQuickAnswer and searchEmoji for Mongo

--- src/main/java/com/bot/repository/Impl/DBRepositoryMongoImpl.java
+++ src/main/java/com/bot/repository/Impl/DBRepositoryMongoImpl.java
@@ -5,6 +5,7 @@ import com.bot.util.MongoDataBase;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import org.telegram.telegrambots.meta.api.objects.Contact;
@@ -12,6 +13,10 @@ import org.telegram.telegrambots.meta.api.objects.Contact;
 @Profile("test")
 @Repository
 public class DBRepositoryMongoImpl implements DBRepository {
+
+    private final String WEATHER_MOOD = "weather_mood";
+    private final String DIALOG_PHRASE = "dialog_phrase";
+
     @Override
     public void addUserInfo(Contact contact, Long chatId) {
         MongoDatabase db = new MongoDataBase().getDataBase();
@@ -27,11 +32,25 @@ public class DBRepositoryMongoImpl implements DBRepository {
 
     @Override
     public String searchQuickAnswer(String phrase) {
-        return null;
+        MongoDatabase db = new MongoDataBase().getDataBase();
+        MongoCollection<Document> collection = db.getCollection(DIALOG_PHRASE);
+        return getValue(phrase, collection);
     }
 
     @Override
     public String searchEmoji(String weatherMood) {
-        return null;
+        MongoDatabase db = new MongoDataBase().getDataBase();
+        MongoCollection<Document> collection = db.getCollection(WEATHER_MOOD);
+        return getValue(weatherMood, collection);
+    }
+
+    private String getValue(String key,  MongoCollection<Document> collectionName){
+        for (Document doc : collectionName.find()) {
+            JSONObject json = new JSONObject(doc.toJson());
+            if(json.has(key)){
+                return json.get(key).toString();
+            }
+        }
+        return "";
     }
 }
