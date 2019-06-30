27d8840b2190aaa2d18a0beb08322a4a3c6d24e4
Ivan Lozovskyi <lozovskii.ivan@gmail.com>
2019-06-30 13:08:44 +0300

    rollback Bot changes

--- src/main/java/com/bot/service/Bot.java
+++ src/main/java/com/bot/service/Bot.java
@@ -1,6 +1,5 @@
 package com.bot.service;
 
-import com.bot.util.GuiServiceUtil;
 import com.bot.util.exception.NoAnswerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Location;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,7 +26,7 @@ public class Bot extends TelegramLongPollingBot {
     @Autowired
     private MessageService messageService;
     @Autowired
-    private GuiServiceUtil guiServiceUtil;
+    private DBService dbService;
 
     @Value("${botName}")
     private String botName;
@@ -33,6 +38,9 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message msg = update.getMessage();
+//        log.info(msg.getContact().getPhoneNumber());
+//        log.info(msg.getContact().getFirstName());
+//        dbService.addUserInfo(msg.getContact(), msg.getChatId());
         String answer = getAnswer(msg);
         sendMsg(msg, answer);
     }
@@ -49,7 +57,7 @@ public class Bot extends TelegramLongPollingBot {
 
     private void sendMsg(Message msg, String text) {
         SendMessage message = new SendMessage();
-        guiServiceUtil.drow(message);
+        setButtons(message);
         message.setChatId(msg.getChatId());
         message.setText(text);
         try {
@@ -65,6 +73,22 @@ public class Bot extends TelegramLongPollingBot {
         } catch (IOException e) {
             throw new NoAnswerException("Failed to get answer for text: " + message.getText(), e);
         }
+    }
+
+    private void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(keyboardMarkup);
+        keyboardMarkup.setSelective(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("Crypto currency"));
+        keyboardFirstRow.add(new KeyboardButton("Currency"));
+
+        keyboard.add(keyboardFirstRow);
+        keyboardMarkup.setKeyboard(keyboard);
     }
 
 }
